d0d17a5e5096c17be2e23d96f03c3bd0e64893f1
AnsahMohammad <mohdansah10@gmail.com>
2023-07-22 09:42:19 +0530

    updated hand tracker

--- Models/gesture_model.py
+++ Models/gesture_model.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import save_model, load_model
 import cv2
 import numpy as np
 
-model = load_model("gesture_detection_model.h5")
+model = load_model("./gesture_detection_model.h5")
 
 categories = {
     "0": 0,
@@ -27,19 +27,22 @@ categories = {
     "19": 19,
 }
 
-new_image_path = "1004.jpg"
-new_img = cv2.imread(new_image_path)
-new_img = cv2.resize(new_img, (50, 50))
-new_img = new_img.astype("float32") / 255.0
-new_img = np.expand_dims(new_img, axis=0)  # Add a batch dimension
+def what_gesture(new_img):
+    # image_path = "1004.jpg"
+    new_img = cv2.resize(new_img, (50, 50))
+    new_img = new_img.astype("float32") / 255.0
+    new_img = np.expand_dims(new_img, axis=0)  # Add a batch dimension
 
-predicted_probabilities = model.predict(new_img)
-predicted_label = np.argmax(predicted_probabilities)
+    predicted_probabilities = model.predict(new_img)
+    predicted_label = np.argmax(predicted_probabilities)
 
-gesture_label = None
-for label, value in categories.items():
-    if value == predicted_label:
-        gesture_label = label
-        break
+    gesture_label = None
+    for label, value in categories.items():
+        if value == predicted_label:
+            gesture_label = label
+            break
 
-print("Predicted gesture label for the new image:", gesture_label)
+    print("Predicted gesture label for the new image:", gesture_label)
+    return gesture_label
+
+# what_gesture(cv2.imread("1004.jpg"))
