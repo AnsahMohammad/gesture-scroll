a201efad98a7a83ee4622517fac14222fbfe5937
AnsahMohammad <mohdansah10@gmail.com>
2023-07-23 14:08:49 +0530

    updated model

--- Models/gesture_model.py
+++ Models/gesture_model.py
@@ -5,26 +5,10 @@ import numpy as np
 model = load_model("./gesture_detection_model.h5")
 
 categories = {
-    "0": 0,
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "11": 11,
-    "12": 12,
-    "13": 13,
-    "14": 14,
-    "15": 15,
-    "16": 16,
-    "17": 17,
-    "18": 18,
-    "19": 19,
+    "down": 0,
+    "neutral": 1,
+    "up": 2,
+    "other": 3
 }
 
 def what_gesture(new_img):
